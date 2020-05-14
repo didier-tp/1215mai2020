5d9237dbd0a59b8402dd2473ce3f80fa37a61266
didier <didier@d-defrance.fr>
2020-05-14 11:22:26 +0200

    daoJpa avec spring

--- monAppliWeb/src/main/java/tp/dao/DeviseDaoJpa.java
+++ monAppliWeb/src/main/java/tp/dao/DeviseDaoJpa.java
@@ -3,69 +3,54 @@ package tp.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import tp.entity.Devise;
 
 //@Component
-@Repository  // @Component de type Repository/DAO
+@Repository // @Component de type Repository/DAO
 //pour demander à ce que cette classe soit prise en charge par Spring
 //par défaut en un seul exemplaire (comme singleton)
 //@Scope("singleton") par défaut : approprié pour une classe de traitement (Service, Dao, ...)
 //id par défaut du composant spring : deviseDaoJpa (nom de classe avec minuscule au debut)
+@Transactional // pour demander à Spring de gérer commit/rollback
 public class DeviseDaoJpa implements DeviseDao {
-	
-	private EntityManager entityManager;
-	
 
-	public void setEntityManager(EntityManager entityManager) {
-		this.entityManager = entityManager;
-	}
+	@PersistenceContext() //pour demander à Spring d'initialiser le entityManager
+	//via META-INF/persistence.xml  , EntityManagerFactory , ...
+	private EntityManager entityManager;
+
 
 	@Override
 	public Devise findDeviseByCode(String code) {
-		Devise d =null;
-		d= entityManager.find(Devise.class, code);
+		Devise d = null;
+		d = entityManager.find(Devise.class, code);
 		return d;
 	}
 
 	@Override
 	public List<Devise> findAllDevise() {
-		return entityManager.createQuery("SELECT d FROM Devise d ",Devise.class)
-				            .getResultList();
+		return entityManager.createQuery("SELECT d FROM Devise d ", Devise.class).getResultList();
 	}
 
 	@Override
 	public void deleteDevise(String code) {
-		try {
-			entityManager.getTransaction().begin();
-				Devise d= entityManager.find(Devise.class, code);
-				entityManager.remove(d);
-			entityManager.getTransaction().commit();
-		} catch (Exception e) {
-			entityManager.getTransaction().rollback();
-			e.printStackTrace();
-		}
+		Devise d = entityManager.find(Devise.class, code);
+		entityManager.remove(d);
+		entityManager.getTransaction().commit();
 	}
 
 	@Override
 	public void saveOrUpdateDevise(Devise d) {
-		try {
-			entityManager.getTransaction().begin();
-				Devise dExistante= entityManager.find(Devise.class, d.getCode());
-				if(dExistante==null) {
-					entityManager.persist(d);
-				}else {
-					entityManager.merge(d);
-				}
-			entityManager.getTransaction().commit();
-		} catch (Exception e) {
-			entityManager.getTransaction().rollback();
-			e.printStackTrace();
+		Devise dExistante = entityManager.find(Devise.class, d.getCode());
+		if (dExistante == null) {
+			entityManager.persist(d);
+		} else {
+			entityManager.merge(d);
 		}
-
 	}
 
 }
