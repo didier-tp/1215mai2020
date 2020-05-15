e994ed360b96eabab012e8759f0255235e4c377f
didier <didier@d-defrance.fr>
2020-05-15 10:44:23 +0200

    s

--- monAppliWeb/src/main/java/tp/service/ServiceCompteImpl.java
+++ monAppliWeb/src/main/java/tp/service/ServiceCompteImpl.java
@@ -2,13 +2,14 @@ package tp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import tp.dao.CompteDao;
 import tp.entity.Compte;
 
 @Service // ou @Component
-@Transactional
+@Transactional(/*propagation = Propagation.REQUIRED par defaut */)
 public class ServiceCompteImpl implements ServiceCompte {
 	
 	@Autowired
@@ -23,11 +24,11 @@ public class ServiceCompteImpl implements ServiceCompte {
 	public void transferer(Double montant, Long numCptDeb, Long numCptCred) {
 		Compte cptDeb = compteDao.findById(numCptDeb);
 		cptDeb.setSolde(cptDeb.getSolde() - montant);
-		compteDao.save(cptDeb);//explicite ou implicite
+		//compteDao.save(cptDeb);//explicite ou implicite en mode persistant
 		
 		Compte cptCred = compteDao.findById(numCptCred);
 		cptCred.setSolde(cptCred.getSolde() + montant);
-		compteDao.save(cptCred);
+		//compteDao.save(cptCred);
 	}
 
 }
